20f8fcfce0164698f78516f19fa3395570d17307
alex <aleksandr.baryshnikov83@gmail.com>
2023-10-06 22:42:10 +0200

    files_question

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,7 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
   belongs_to :user
 
-  validates :title, :body, presence: true
+  has_many_attached :files
+
+  validates :title, :body, presence: true 
 end
