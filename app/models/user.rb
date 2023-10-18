e5fd2789b6865c5d3b2eb5a6abb306e7f31a265e
alex <aleksandr.baryshnikov83@gmail.com>
2023-10-19 00:30:32 +0200

    confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-        :omniauthable, omniauth_providers: [:github]
+        :recoverable, :rememberable, :validatable, :confirmable, 
+         :omniauthable, omniauth_providers: %i(github vkontakte)
 
   has_many :questions, dependent: :destroy
   has_many :answers, dependent: :destroy
@@ -20,7 +20,7 @@ class User < ApplicationRecord
   end
 
   def self.find_for_oauth(auth)
-    Services::FindForOauth.new(auth).call
+    FindForOauth.new(auth).call
   end
 
   def create_authorization(auth)
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -27,24 +27,22 @@ Rails.application.configure do
 
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
-
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
   config.action_cable.disable_request_forgery_protection = true
+  OmniAuth.config.test_mode = true
 
-  # Store uploaded files on the local file system in a temporary directory.
+  # Store uploaded files on the local file system in a temporary directory
   config.active_storage.service = :test
-
-  config.action_mailer.perform_caching = false
-
-  # Tell Action Mailer not to deliver emails to the real world.
+@@ -38,6 +39,8 @@
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3001 }
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
-
-  # Raises error for missing translations.
+  # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
